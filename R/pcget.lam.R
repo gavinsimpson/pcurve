a6caf5bb190cac5236f30c3c7103e48b8b2e6661
Gavin Simpson <ucfagls@gmail.com>
2015-01-04 12:23:39 -0600

    set type for all args passed to getlam C fun

--- R/pcget.lam.R
+++ R/pcget.lam.R
@@ -1,43 +1,45 @@
-"pcget.lam"<-
-function(x, s, latent = FALSE, stretch = 2, uni.lam, tag)
-{
-	storage.mode(x) <- "double"
-	storage.mode(s) <- "double"
-	storage.mode(stretch) <- "double"
-	storage.mode(uni.lam) <- "double"
-	if(!missing(tag))
-		s <- s[tag,  ]
-	np <- dim(x)
-	if(length(np) != 2)
-		stop("get.lam needs a matrix input")
-	n <- np[1]
-	p <- np[2]	#
-#
-#  Transpose matrices for C -- stored by rows not cols !!!!!!!!!! and 'transpose' back !!
-#
-	tt <- .C("getlam",
-		n,
-		p,
-		t(x),
-		s = t(x),
-                latent = as.integer(latent),
-		lambda = double(n),
-		tag = integer(n),
-		dist = double(n),
-		as.integer(nrow(s)),
-		t(s),
-		stretch,
-		uni.lam,
-		double(p),
-		double(p), PACKAGE = "pcurve")[c("s", "tag", "lambda", "dist")]
-	if(latent) {
-		tt$lambda <- (tt$lambda - min(tt$lambda))/diff(range(tt$lambda)
-			)
-		tt$tag <- order(tt$lambda)
-	}
-	tt$s <- t(matrix(as.vector(tt$s), nrow = p, ncol = n))
-	tt$dist <- sum(tt$dist)
-	class(tt) <- "principal.curve"
-	tt
+"pcget.lam"<- function(x, s, latent = FALSE, stretch = 2, uni.lam,
+                       tag) {
+    storage.mode(x) <- "double"
+    storage.mode(s) <- "double"
+    storage.mode(stretch) <- "double"
+    storage.mode(uni.lam) <- "double"
+    if(!missing(tag))
+        s <- s[tag,  ]
+    np <- dim(x)
+    if(length(np) != 2)
+        stop("get.lam needs a matrix input")
+    n <- np[1]
+    p <- np[2]	#
+    xt <- t(x)
+    st <- t(s)
+    ##
+    ##  Transpose matrices for C -- stored by rows not cols !!!!!!!!!! and 'transpose' back !!
+    ##
+    tt <- .C("getlam",
+             nn = as.integer(n),
+             pp = as.integer(p),
+             x = as.double(xt),
+             s = as.double(xt),
+             latent = as.integer(latent),
+             lambda = double(n),
+             tag = integer(n),
+             dist = double(n),
+             nns = as.integer(nrow(s)),
+             s = as.double(st),
+             stretch = as.double(stretch),
+             uni.lam = as.double(uni.lam),
+             vecx = double(p),
+             tempsx = double(p),
+             PACKAGE = "pcurve")[c("s", "tag", "lambda", "dist")]
+    if(latent) {
+        tt$lambda <- (tt$lambda - min(tt$lambda))/diff(range(tt$lambda)
+                                                       )
+        tt$tag <- order(tt$lambda)
+    }
+    tt$s <- t(matrix(as.vector(tt$s), nrow = p, ncol = n))
+    tt$dist <- sum(tt$dist)
+    class(tt) <- "principal.curve"
+    tt
 }
 
